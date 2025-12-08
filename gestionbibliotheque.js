61b34625e81f38a4cf64b07ebd7280ebc650a21b
djoemahamoudou-arch <djoemahamoudou@gmail.com>
2025-12-08 11:00:15 +0300

    Mon premier codage sur javascript

---
+++ gestionbibliotheque.js
@@ -0,0 +1,18 @@
+
+let totalLivres = 500;
+    totalLivres +=50;
+    totalLivres -=10;
+    totalLivres +=5;
+
+
+
+console.log(totalLivres)
+
+
+
+ 
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,58 @@
+// Ici, je rédige mon script javascript
+ let nom="Mahamoudou";
+        let prénom="Rachid";
+        let age = 18;
+        console.log("bonjour,je m'appelle ",nom,prénom,"j'ai",age,"ans");
+        
+        let prix = 5;
+        const nombreDcimal = 3.4123;
+        const nombreNegatif = -509;
+        const nombreFraction = 1/3;
+        
+
+
+        console.log("tomate :",prix,"euro")
+        console.log("nombre decimal :",nombreDcimal)
+        console.log("nombre négatif :",nombreNegatif)
+        console.log("nombre fraction :",nombreFraction)
+
+
+        let salutation = "bonjour les gens";
+        let greeting = "Good morning";
+        let andabu = `Kwezi`;
+        let village = 'M\'tsahara';
+        let commune = "M'tsamboro .";
+        console.log(salutation,greeting,andabu,"je suis de",village,"dans la commune de",commune);
+        let vrai = true;
+        let est_faux = false;
+        console.log(vrai,est_faux);
+        
+        
+        
+        
+        let eleve = ["Said ","Amina","bio","bacar"];
+        let diplome = [true,10,"daniel"];
+        eleve[0]; //Récupère Said
+        eleve[1] //Récupère Amina
+        eleve[2] //récupère bao
+        eleve[3] //récupère bacar
+        console.log(eleve);
+        console.log[eleve[0]]; /*récupère l'élève 0*/
+        console.log[eleve[1]]; /*récupère l'élève 1*/
+        console.log[eleve[2]]; /*récupère l'élève 2*/
+        console.log[eleve[3]]; /*récupère l'élève 3*/
+        
+        /*l'objet qui permet de stocker des informations plus complexes qu'une simple liste*/
+        /*A l'intérieur d'un objet , il y a une ou plusieurs Propriétés.chaque propriéteé est associée à une valeur*/
+        
+        
+        let client = {
+            nom: "bacar",
+            village:"koungou",
+            age:25,
+            scores:[40,2]
+            
+        };
+
+
+       
