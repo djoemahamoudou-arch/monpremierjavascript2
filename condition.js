936e7b68c110f8fdcf8334b368c819df2492fe00
djoemahamoudou-arch <djoemahamoudou@gmail.com>
2025-12-09 16:15:04 +0300

    ajoute de mon nvll fichier de condition

---
+++ condition.js
@@ -0,0 +1,45 @@
+// les condition
+
+
+
+let nombreFruit = 19;
+
+// J'affiche l'alert si le nombre de fruit est égal à 18
+
+if (nombreFruit == 18) {
+    alert("vous avait atteint 18 fruits.");
+};
+
+
+
+
+let noteEleve = 10 ;
+if(noteEleve >= 10) {
+    alert(" Vous être admis avec une note de " + noteEleve);
+};
+
+
+
+let ageTelespectateur = 18;
+if (ageTelespectateur >= 18) {
+    alert("vous pouvez regarder rambo ! car vous avez " + ageTelespectateur);
+};
+
+
+
+let age3 = 18 ;
+if (age3 <= 18) {  // si
+    alert("vous pouvez regarder le roie lion ! car vous avait " + age3);
+};
+
+
+let pointPermis = 12;
+if(pointPermis == 12){  //si
+      alert("Vous avez un bonus !");
+    
+
+} else{ // sinon
+    alert("Vous avez des malus");
+}
+
+    
