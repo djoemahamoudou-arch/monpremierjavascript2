9e9811b4681c3117b59cc9b4b62904cfa2175275
djoemahamoudou-arch <djoemahamoudou@gmail.com>
2025-12-17 16:50:36 +0100

    ajoute nvll

--- evenement.js
+++ evenement.js
@@ -12,3 +12,23 @@ const maDate = new Date();
     elementBoutonValiderDate2.textContent = maDate.toDateString();
 });
 
+// 1. Tout d'abord , je récupère l'élement html qui sera associé a l'évenement keydown
+let elementkeydown = document.querySelector("#boutonflech");
+// 2.J'associe l'évenement KEYDOWN mon élément html en utilisant la méthode addEventListener(). A l'intérieur de la méthod eAddEvent listener(), je passe une fonction fléchée comme suit '() => {}'
+
+
+elementkeydown.addEventListener("keydown", (event) => {
+    console.log("#je suis dans l'évenement keydown");
+    console.log("clé event :" ,event.key);
+});
+
+let elementInputNom2 = document.querySelector("#nom");
+let messageSalutation; // je déclare une variable vide.
+let elementSpanNomUtilisateur = document.querySelector("#messageUtilisateur");
+elementSpanNomUtilisateur.textContent = messageSalutation;
+
+
+elementInputNom2.addEventListener("change" , (event) => {
+    console.log(event.target.value);
+    messageSalutation = `Bonjour ${event.target.vallue}`;
+});
